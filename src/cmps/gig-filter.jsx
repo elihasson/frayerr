72e46121914f350afc197acc6dc7b10c162b6ec2
aharonmilman <milmana@gmail.com>
2022-09-16 01:36:43 +0300

    started to work on the header and header components not finished

--- src/cmps/gig-filter.jsx
+++ src/cmps/gig-filter.jsx
@@ -1,5 +1,33 @@
+import React, { useState, useEffect } from 'react'
+
+import SearchIcon from '@mui/icons-material/Search';
+
 export const GigFilter = () => {
+
+    const [txt, setTxt] = useState('');
+    
+    const handleChange = ({ target }) => {
+        const { value } = target
+        setTxt(value);
+    }
+    
+    const onSubmit = (ev) => {
+        ev.preventDefault();
+        console.log('onSubmit filter line12 gig-filter:', txt );
+    }
     return (
-        <h1>Hi From gig Filter</h1>
+            <form className='search-bar-container'
+                onSubmit={onSubmit}>
+                <input
+                    className='search-bar'
+                    type="search"
+                    placeholder="Find services"
+                    autoComplete='off'
+                    value={txt}
+                    name='txt'
+                    onChange={handleChange}
+                />
+                <button className='search-button'><SearchIcon /></button>
+            </form>
     )
 }
