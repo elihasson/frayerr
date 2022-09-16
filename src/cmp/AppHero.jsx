09dc5a4a826026533332ee1b82b63c552727f778
Ariel201711 <arielk216@gmail.com>
2022-09-16 17:22:05 +0300

    Continue Working on hero cmp styles

--- src/cmp/AppHero.jsx
+++ src/cmp/AppHero.jsx
@@ -1,13 +1,24 @@
-import logo from '../assets/img/bg-hero-1.jpg'
+import logo from '../assets/img/hero-portrait_1.png'
 
 export const AppHero = () => {
     return (
-        <div className="hero-container debug">
-            <img src={logo} alt="Hero" className='hero-img' />
-            <div className='hero-content debug'>
-                <h1>
-                    Find the perfect <i>freelance</i> services for your business
-                </h1>
+        <div className='hero-container debug'>
+            <div className='hero-imgs-container debug'>
+                <div className='hero-img-container flex debug'>
+                    <img src={logo} alt='Hero' className='hero-img debug' />
+                </div>
+            </div>
+            <div className='hero-content-container flex max-width-container debug'>
+                <div className='hero-content debug'>
+                    <h1>
+                        Find the perfect <i>freelance</i> services for your business
+                    </h1>
+                </div>
+                {/* <div className="static-background"></div> */}
+                <div className='seller-name debug'>
+                    <span className='seller-skill'>Andrea, </span>
+                    <span className='seller-skill'>Web Designer</span>
+                </div>
             </div>
         </div>
     )
