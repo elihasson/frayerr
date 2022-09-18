10a27ba56a091d3e8ae68353aad7455c17e621b4
aharonmilman <milmana@gmail.com>
2022-09-18 10:51:56 +0300

    carousel fixed require path for CSS error

--- src/cmps/carousel-item.jsx
+++ src/cmps/carousel-item.jsx
@@ -1,20 +1,10 @@
 export const CarouselItem = ({ children, width, imgUrl }) => {
-    // debugger
     // const itemStyle = { width: width, backgroundImage: `url(https://robohash.org/1})` }
-    // const itemStyle = { width: width, backgroundImage: `url(${imgUrl})`` }
-    const image =  imgUrl.imgUrl
-    
-    //  const itemStyle = { width: width, backgroundImage: `url(` + require (`../assets/image/Pst_Brian.jpg`) + ')'}
-    // <img src={require('../images/' + fileNameExt)} />
+    let image =  imgUrl.imgUrl
+   
+    const itemStyle = { width: width, backgroundImage: 'url(' + require ('../assets/images/' + image) + ')'}
 
-    console.log('Hello123 ',image)
-    // debugger this
-    const itemStyle = { width: width, backgroundImage: 'url(' + require ('../assets/' + image) + ')'}
-
-    // const itemStyle = { width: width, backgroundImage:  `url('' + image})`}
-    // `url(${templateStringForImage})`
     console.log(itemStyle)
-    // debugger
     return (
         <div className="carousel-item" style={itemStyle} >
             {children}
