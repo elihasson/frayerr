c89fab6ee64d4e32cb6132116aee89c10fb33c21
Ariel201711 <arielk216@gmail.com>
2022-09-18 03:40:17 +0300

    added a hero search bar with some style and a hero details with initial style

---
+++ src/cmps/hero-details.jsx
@@ -0,0 +1,5 @@
+export const HeroDetails = () => {
+    return (
+        <div className='hero-details' style={{fontSize: '30px', color: '#eee'}}>Andrea, Web Designer</div>
+    )
+}
--- src/cmps/search-bar.jsx
+++ src/cmps/search-bar.jsx
@@ -1,7 +1,34 @@
+import React, { useState, useEffect } from 'react'
+import SearchIcon from '@mui/icons-material/Search';
+
 export const SearchBar = () => {
+
+    const [txt, setTxt] = useState('')
+
+    const handleChange = ({ target }) => {
+        const { value } = target
+        setTxt(value)
+    }
+
+    const onSubmit = (ev) => {
+        ev.preventDefault();
+        console.log('onSubmit filter line12 gig-filter:', txt);
+    }
+
     return (
-        <form>
-            
+        <form className='gig-search-bar-container' onSubmit={onSubmit}>
+            <span className='search-icon-container'>
+                <SearchIcon />
+            </span>
+            <input
+                className='gig-search-bar'
+                type='search'
+                value={txt}
+                onChange={handleChange}
+                placeholder='Try "building web app"'
+                autoComplete='off'
+            />
+            <button className='gig-btn-search'>Search</button>
         </form>
     )
 }
