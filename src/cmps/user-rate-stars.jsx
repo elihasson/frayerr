cb627b97573969af392ad8f6ef3fab7d6539fdff
eli hasson <elihasson@gmail.com>
2022-09-20 19:58:32 +0300

    gig details categories

--- src/cmps/user-rate-stars.jsx
+++ src/cmps/user-rate-stars.jsx
@@ -3,14 +3,14 @@ import StarIcon from '@mui/icons-material/Star';
 import StarOutlineIcon from '@mui/icons-material/StarOutline';
 
 export function UserRateStars({ gig }) {
-    const stars = [<StarIcon />, <StarIcon />, <StarIcon />, <StarIcon />, <StarIcon />]
+    const stars = [<StarIcon fontSize="small" />, <StarIcon fontSize="small"/>, <StarIcon fontSize="small"/>, <StarIcon fontSize="small"/>, <StarIcon fontSize="small"/>]
 
     if (!gig.owner) return <React.Fragment></React.Fragment>
     return (
         <div className='stars'>
             {stars.map((star, idx) => {
                 if (idx < (gig.owner.rate)) return <span key={idx} className='star'>{star}</span>
-                else return <span key={idx} className='star'><StarOutlineIcon /></span>
+                else return <span key={idx} className='star fs14'><StarOutlineIcon fontSize="small"/></span>
             })}
         <span className='rate'>{gig.owner.rate}</span>
         </div>
