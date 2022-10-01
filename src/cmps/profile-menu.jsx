182475e163e5e920c721ee8c41a282fe46c39ac7
Ariel201711 <arielk216@gmail.com>
2022-10-01 16:11:32 +0300

    added home and explore navigations in user profile menu

--- src/cmps/profile-menu.jsx
+++ src/cmps/profile-menu.jsx
@@ -7,12 +7,29 @@ export const ProfileMenu = ({ onLogout, user, closeProfileMenu }) => {
             <ul className="clean-list profile-scroll">
                 <li className="menu-item" onClick={() => closeProfileMenu()}>
                     {/* currently /user/${user._id} instead of profile/${user._id} */}
-                    <NavLink className="clean-link" to={`/user/${user._id}`}>Profile</NavLink></li>
+                    <NavLink className="clean-link" to={`/`}>Home
+                    </NavLink>
+                </li>
+                <li className="menu-item" onClick={() => closeProfileMenu()}>
+                    {/* currently /user/${user._id} instead of profile/${user._id} */}
+                    <NavLink className="clean-link" to={`/explore`}>Explore
+                    </NavLink>
+                </li>
+                <li className="menu-item" onClick={() => closeProfileMenu()}>
+                    {/* currently /user/${user._id} instead of profile/${user._id} */}
+                    <NavLink className="clean-link" to={`/user/${user._id}`}>Profile
+                    </NavLink>
+                </li>
                 <li className="menu-item" onClick={() => closeProfileMenu()}>
                     {/* currently /user/${user._id} instead of dashboard/${user._id} */}
                     <NavLink
-                        className="clean-link" to={`/user/${user._id}`}>Dashboard</NavLink></li>
-                <li className="menu-item logout" onClick={() => onLogout()}><NavLink className="clean-link" to={`/`}>Logout</NavLink></li>
+                        className="clean-link" to={`/user/${user._id}`}>Dashboard
+                    </NavLink>
+                </li>
+                <li className="menu-item logout" onClick={() => onLogout()}>
+                    <NavLink className="clean-link" to={`/`}>Logout
+                    </NavLink>
+                </li>
             </ul>
         </aside>
     )
