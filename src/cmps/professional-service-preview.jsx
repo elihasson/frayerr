f629762dd1c420f2f719a3ed5c6d5f5351721df8
Ariel201711 <arielk216@gmail.com>
2022-10-08 05:15:07 +0300

    slicing first 5 professional services including bug fixes - adding a unique key for every preview

--- src/cmps/professional-service-preview.jsx
+++ src/cmps/professional-service-preview.jsx
@@ -1,12 +1,14 @@
 export const ProfessionalServicePreview = ({ category, onSetFilter }) => {
-    <div className='img-container' onClick={() => {
-        onSetFilter(category?.title)
-    }}>
-        <span className="on-click"></span>
-        <div className="category-container">
-            <div className="subtitle">{category?.subtitle}</div>
-            <div className="title">{category?.title}</div>
+    return (
+        <div className='img-container' onClick={() => {
+            onSetFilter(category?.title)
+        }}>
+            <span className="on-click"></span>
+            <div className="category-container">
+                <div className="subtitle">{category?.subtitle}</div>
+                <div className="title">{category?.title}</div>
+            </div>
+            <img src={category?.imgUrl} />
         </div>
-        <img src={category?.imgUrl} />
-    </div>
+    )
 }
--- src/cmps/professional-services.jsx
+++ src/cmps/professional-services.jsx
@@ -3,82 +3,8 @@ import { ProfessionalServicePreview } from './professional-service-preview.jsx'
 export const ProfessionalServices = ({categories, onSetFilter}) => {
     return (
         <div className='professional-services'>
-            {categories.slice(10).map(category => <ProfessionalServicePreview category={category} onSetFilter={onSetFilter}/>)}
-            {/* <div className='img-container' onClick={() => {
-                    onSetFilter(categories[0]?.title)
-                }}>
-                    <span className="on-click"></span>
-                    <div className="category-container">
-                        <div className="subtitle">{categories[0]?.subtitle}</div>
-                        <div className="title">{categories[0]?.title}</div>
-                    </div>
-                    <img src={categories[0].imgUrl} />
-                </div>
-                <div className='img-container' onClick={() => {
-                    onSetFilter(categories[1]?.title)
-                }}>
-                    <span className="on-click"></span>
-                    <div className="category-container">
-                        <div className="subtitle">{categories[1]?.subtitle}</div>
-                        <div className="title">{categories[1]?.title}</div>
-                    </div>
-                    <img src={categories[1].imgUrl} />
-
-                </div>
-                <div className='img-container' onClick={() => {
-                    onSetFilter(categories[2]?.title)
-                }}>
-                    <span className="on-click"></span>
-                    <div className="category-container">
-
-                        <div className="subtitle">{categories[2]?.subtitle}</div>
-                        <div className="title">{categories[2]?.title}</div>
-                    </div>
-
-                    <img src={categories[2].imgUrl} />
-                </div>
-                <div className='img-container' onClick={() => {
-                    onSetFilter(categories[3]?.title)
-                }}>
-                    <span className="on-click"></span>
-                    <div className="category-container">
-
-                        <div className="subtitle">
-                            {categories[3]?.subtitle}
-                        </div>
-                        <div className="title">{categories[3]?.title}</div>
-                    </div>
-
-                    <img src={categories[3].imgUrl} />
-                </div>
-                <div className='img-container' onClick={() => {
-                    onSetFilter(categories[4]?.title)
-                }}>
-                    <span className="on-click"></span>
-                    <div className="category-container">
-
-                        <div className="subtitle">
-                            {categories[4]?.subtitle}
-                        </div>
-                        <div className="title">{categories[4]?.title}</div>
-                    </div>
-
-                    <img src={categories[4].imgUrl} />
-                </div>
-                <div className='img-container' onClick={() => {
-                    onSetFilter(categories[5]?.title)
-                }}>
-                    <span className="on-click"></span>
-                    <div className="category-container">
-
-                        <div className="subtitle">
-                            {categories[5]?.subtitle}
-                        </div>
-                        <div className="title">{categories[5]?.title}</div>
-                    </div>
-
-                    <img src={categories[5].imgUrl} />
-                </div> */}
+            {categories.slice(0, 5).map(category => <ProfessionalServicePreview key={category.title} category={category} onSetFilter={onSetFilter}/>)}
+            {/* later, when I'll have a carousel, I'll change the higher bound to 10 */}
         </div>
     )
 }
