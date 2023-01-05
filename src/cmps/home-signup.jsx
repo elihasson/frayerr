49086e3b8ca58eadaa63a2accd4578ad97a3017b
Ariel201711 <arielk216@gmail.com>
2023-01-05 23:13:45 +0200

    added a signup button to home-signup cmp and changed cmp height, inserted title and button into another div

--- src/cmps/home-signup.jsx
+++ src/cmps/home-signup.jsx
@@ -1,7 +1,17 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { toggleJoinModal } from '../store/system.actions'
+
 export const HomeSignup = () => {
+    const user = useSelector(state => state.userModule.user)
+    const dispatch = useDispatch()
+
     return (
-        <section className='home-signup main-layout'>
-            <div className='home-signup-txt'><h2>Find the <i>talent</i> needed to get your business <i>growing.</i></h2></div>
+        !user &&
+        <section className='home-signup-container main-layout'>
+            <div className='home-signup-content'>
+                <h2>Find the <i>talent</i> needed to get your business <i>growing.</i></h2>
+                <button className='btn' onClick={() => { dispatch(toggleJoinModal()) }}>Get Started</button>
+            </div>
         </section>
     )
 }
